cad647244593db902cbe573f42e0ab3c01de3b5b
Nikita-Bhoyar <nikitabhoyar99@gmail.com>
2025-10-16 23:22:14 +0530

    Add files via upload

---
+++ Variable_Input_Prog.py
@@ -0,0 +1,16 @@
+
+#Fname = input ("Enter your First name: ")
+#Lname = input ("Enter your Last name: ")
+#print("Hello", Fname , Lname)
+
+
+Fname = input("What is your FirstName: ")
+print("Here is the First name",Fname)
+
+Lname = input ("What is your Lastname: ")
+print("Here is the Lastname",Lname)
+
+Surname = input ("What is your Whole Family name: ")
+print("The whole family name is: ", Surname)
+
+
