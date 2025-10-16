106ab89708fb6774bc830040fb92efd6f9bdd85f
Nikita-Bhoyar <nikitabhoyar99@gmail.com>
2025-10-16 23:29:27 +0530

    Add files via upload
    The code is about calculator program,

---
+++ Calculator_Prog.py
@@ -0,0 +1,24 @@
+num1 = int (input("Enter the first number:"))
+num2 = int (input("Enter the Second number:"))
+
+print(type(num1))
+print(type(num2))
+
+Sum_of_two_numbers = num1 + num2
+print("Addition of two numbers is: ", Sum_of_two_numbers)
+
+Subtraction_of_two_numbers = num1 - num2
+print("Subtraction of two numbers is: ", Subtraction_of_two_numbers)
+
+Multiplication_of_two_numbers = num1 * num2 
+print("Multiplication of two numbers is: ", Multiplication_of_two_numbers)
+
+Division_of_two_numbers = num1 / num2 
+print("Division of two numbers is: ", Division_of_two_numbers)
+
+Modulous_of_two_numbers = num1 % num2 
+print("Modulous of two numbers is: ",Modulous_of_two_numbers)
+
+
+
+
